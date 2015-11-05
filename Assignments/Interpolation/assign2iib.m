e364f52b43a9384c1fa83b420632d7379664dc74
Aditya <adityagupta1089@gmail.com>
2015-11-05 15:51:35 +0530

    changes

--- Assignments/Interpolation/assign2iib.m
+++ Assignments/Interpolation/assign2iib.m
@@ -6,7 +6,7 @@ y=[1 1.2214 1.4918 1.8221 2.2255 2.7177 3.3166 4.0426 4.9185 5.9694];
 n=numel(x);
 h=0.1;
 z=1.75;
-delta =zeros(n+1);
+delta =zeros(n);
 u=(z-x(1))/h;
 for i=1:n
     delta(1,i)=y(i);
@@ -17,11 +17,11 @@ for i=2:n
     end
 end
 f=g(x(1));
-for i=1:n
+for i=1:n-1
     y=1;
     for j=1:i
         y=y*(u-(j-1))/j;
     end    
-    f=f+y*delta(i+1,i+1);
+    f=f+(-1)^k*y*delta(i+1,i+1);
 end
 fprintf('f(%f) = %f\n',z,f);
---
+++ Assignments/Interpolation/assign2iv.m
@@ -0,0 +1,24 @@
+clc
+clear all
+x=[0 0.1 0.3 0.6 1.0];
+y=[-6 -5.89483 -5.65014 -5.17788 -4.28172];
+n=numel(x);
+F=zeros(n);
+z=0.85;
+for i=1:n
+    F(1,i)=y(i);
+end
+for i=2:n
+    for j=2:i
+        F(i,j)=(F(j-1,i)-F(i-1,j-1))/(x(i)-x(i-j+1));
+    end
+end
+f=0;
+for i=1:n
+    y=1;
+    for j=1:i
+        y=y*(z-x(i));
+    end
+    f=f+y*F(i,i);
+end
+fprintf('f(%f)=%f\n',z,f);
